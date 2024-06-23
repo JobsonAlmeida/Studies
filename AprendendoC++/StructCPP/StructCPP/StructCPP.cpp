90d36279a8d599ea6d624a5bb64d0ac4deda5e51
Jobson Almeida <jobsonbox@gmail.com>
2024-06-23 09:33:03 -0300

    complete exercise

--- AprendendoC++/StructCPP/StructCPP/StructCPP.cpp
+++ AprendendoC++/StructCPP/StructCPP/StructCPP.cpp
@@ -2,24 +2,50 @@
 
 class Data
 {
-
 public:
+	static int contador;
+	int id;
 	int Dia;
 	int Mes;
 	int Ano;
-	Data(): Dia(22), Mes(07), Ano(2013){} //this is just a way to type the constructor and does this.Dia = Dia this.Mes = Mes this.Ano = ano
+	
+	Data() { 
+		id = contador++;
+		std::cout << "\nConstrutor Data(" << id << ") executado" << "\n";
+		std::cout << "contador: " << contador << "\n";
+	}
+	~Data() {
+		std::cout << "\nDestrutor ~Data(" << id << ") executado" << "\n";
+		std::cout << "contador: " << contador << "\n";
+	}
+
 };
 
-
-
+static int MinhaEstaticaGlobal = 2;
 int main()
 {
-	Data Agenda;
-	Agenda.Dia = 22;
-	Agenda.Mes = 07;
-	Agenda.Ano = 2013;
-	std::cout << "Dia: " << Agenda.Dia << " Mes: " << Agenda.Mes << " Ano: " << Agenda.Ano << "\n";
-	system("pause");
+	static int MinhaEstatica1 = 1; //ok
 
+	{
+		Data Agenda1;
+		static int MinhaEstatica2 = 2;
+		std::cout << "MinhaEstatica1: " << MinhaEstatica1 << "\n";
+		//Escopo 1
+		//id = 1
+		//contador = 2
+
+		{
+
+			Data Agenda2;
+			Data* Agenda3 = new Data;
+		}
+
+		Data Agenda4;		 
+	}
+
+	std::cout << "MinhaEstatica1: " << MinhaEstatica1 << "\n";
+	std::cout << "MinhaEstatica2Global: " << MinhaEstaticaGlobal << "\n";
+	
+	system("pause");
 	return 0;
 }
